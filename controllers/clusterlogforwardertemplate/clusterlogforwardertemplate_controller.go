7cf0e0356d232eae789a5ef5ed3ff4c7cbe1d87d
Bo Meng <bmeng@redhat.com>
2023-09-12 15:57:27 +0800

    implement the HLF controller

--- controllers/clusterlogforwardertemplate/clusterlogforwardertemplate_controller.go
+++ controllers/clusterlogforwardertemplate/clusterlogforwardertemplate_controller.go
@@ -19,8 +19,6 @@ package clusterlogforwardertemplate
 import (
 	"context"
 	"fmt"
-	"strconv"
-	"strings"
 
 	"github.com/go-logr/logr"
 	loggingv1 "github.com/openshift/cluster-logging-operator/apis/logging/v1"
@@ -35,15 +33,9 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/predicate"
 
 	hlov1alpha1 "github.com/openshift/hypershift-logging-operator/api/v1alpha1"
+	"github.com/openshift/hypershift-logging-operator/pkg/clusterlogforwarder"
+	"github.com/openshift/hypershift-logging-operator/pkg/consts"
 	"github.com/openshift/hypershift-logging-operator/pkg/hostedcluster"
-)
-
-const (
-	// Same as the CLO, we set the CLFT as signleton here
-	singletonName = "instance"
-	// ManagedLogForwardTemplatePrefix prefix to identify the entries in the array are managed by template
-	ManagedLogForwardTemplatePrefix = "openshift-sre"
-	TemplateFinalizer               = "logging.managed.openshift.io"
 )
 
 // ClusterLogForwarderTemplateReconciler reconciles a ClusterLogForwarderTemplate object
@@ -65,8 +57,8 @@ func (r *ClusterLogForwarderTemplateReconciler) Reconcile(
 ) (ctrl.Result, error) {
 	r.log = ctrllog.FromContext(ctx).WithName("controller")
 
-	if req.NamespacedName.Name != singletonName {
-		err := fmt.Errorf("clusterLogForwarderTemplate name must be '%s'", singletonName)
+	if req.NamespacedName.Name != consts.SingletonName {
+		err := fmt.Errorf("clusterLogForwarderTemplate name must be '%s'", consts.SingletonName)
 		r.log.V(1).Error(err, "")
 		return ctrl.Result{}, err
 	}
@@ -87,25 +79,25 @@ func (r *ClusterLogForwarderTemplateReconciler) Reconcile(
 
 	if !template.DeletionTimestamp.IsZero() {
 		deletion = true
-		controllerutil.RemoveFinalizer(template, TemplateFinalizer)
+		controllerutil.RemoveFinalizer(template, consts.ManagedLoggingFinalizer)
 		err = r.Client.Update(ctx, template)
 		if err != nil {
 			return ctrl.Result{}, err
 		}
 	} else {
 		deletion = false
-		controllerutil.AddFinalizer(template, TemplateFinalizer)
+		controllerutil.AddFinalizer(template, consts.ManagedLoggingFinalizer)
 		err = r.Client.Update(ctx, template)
 		if err != nil {
 			return ctrl.Result{}, err
 		}
 	}
 
-	clusterLogForwarder := &loggingv1.ClusterLogForwarder{}
+	clf := &loggingv1.ClusterLogForwarder{}
 
 	for _, hcp := range hcpList {
 		found := false
-		err := r.Get(ctx, types.NamespacedName{Name: "instance", Namespace: hcp.Namespace}, clusterLogForwarder)
+		err := r.Get(ctx, types.NamespacedName{Name: "instance", Namespace: hcp.Namespace}, clf)
 		if err != nil && errors.IsNotFound(err) {
 			found = false
 		} else if err == nil {
@@ -121,8 +113,8 @@ func (r *ClusterLogForwarderTemplateReconciler) Reconcile(
 			}
 			// If CLFT is being deleted, and CLF found, clean up the CLF with all the CLFT entries
 			if found {
-				clusterLogForwarder = r.CleanUpClusterLogForwarder(clusterLogForwarder)
-				err = r.Update(ctx, clusterLogForwarder)
+				clusterlogforwarder.CleanUpClusterLogForwarder(clf, consts.ProviderManagedRuleNamePrefix)
+				err = r.Update(ctx, clf)
 				if err != nil {
 					return ctrl.Result{}, err
 				}
@@ -131,27 +123,27 @@ func (r *ClusterLogForwarderTemplateReconciler) Reconcile(
 		}
 		if !deletion {
 			// For any reconcile loop, clean up the CLF for entries from CLFT, and build from new CLFT
-			clusterLogForwarder.Name = "instance"
-			clusterLogForwarder.Namespace = hcp.Namespace
+			clf.Name = "instance"
+			clf.Namespace = hcp.Namespace
 
 			//TODO: Need to fix the serviceaccount name here once we know how to use it
 			//clusterLogForwarder.Spec.ServiceAccountName = template.Spec.Template.ServiceAccountName
 
-			r.CleanUpClusterLogForwarder(clusterLogForwarder)
-			r.BuildInputs(clusterLogForwarder, template)
-			r.BuildOutputs(clusterLogForwarder, template)
-			r.BuildPipelines(clusterLogForwarder, template)
+			clusterlogforwarder.CleanUpClusterLogForwarder(clf, consts.ProviderManagedRuleNamePrefix)
+			clf = clusterlogforwarder.BuildInputsFromTemplate(template, clf)
+			clf = clusterlogforwarder.BuildOutputsFromTemplate(template, clf)
+			clf = clusterlogforwarder.BuildPipelinesFromTemplate(template, clf)
 
 			// Found existing CLF, update with new CLFT
 			if found {
-				if err := r.Update(context.TODO(), clusterLogForwarder); err != nil {
+				if err := r.Update(context.TODO(), clf); err != nil {
 					return ctrl.Result{}, err
 				}
 				return ctrl.Result{}, nil
 			}
 			// No CLF found, create with the CLFT entries
 			if !found {
-				if err := r.Create(ctx, clusterLogForwarder); err != nil {
+				if err := r.Create(ctx, clf); err != nil {
 					return ctrl.Result{}, err
 				}
 				return ctrl.Result{}, nil
@@ -160,88 +152,6 @@ func (r *ClusterLogForwarderTemplateReconciler) Reconcile(
 	}
 
 	return ctrl.Result{}, nil
-}
-
-// BuildInputs builds the input array from the template and adds to the clean log forwarder
-func (r *ClusterLogForwarderTemplateReconciler) BuildInputs(clusterLogForwarder *loggingv1.ClusterLogForwarder,
-	logForwarderTemplate *hlov1alpha1.ClusterLogForwarderTemplate) {
-
-	// Build the input from template
-	if len(logForwarderTemplate.Spec.Template.Inputs) < 1 {
-		r.log.V(3).Info("there is no input defined, skipping...")
-	} else {
-		for _, tInput := range logForwarderTemplate.Spec.Template.Inputs {
-			if !strings.Contains(tInput.Name, ManagedLogForwardTemplatePrefix) {
-				tInput.Name = ManagedLogForwardTemplatePrefix + "-" + tInput.Name
-			}
-			clusterLogForwarder.Spec.Inputs = append(clusterLogForwarder.Spec.Inputs, tInput)
-		}
-	}
-}
-
-// BuildOutputs builds the output array from the template and adds to the clean log forwarder
-func (r *ClusterLogForwarderTemplateReconciler) BuildOutputs(clusterLogForwarder *loggingv1.ClusterLogForwarder,
-	logForwarderTemplate *hlov1alpha1.ClusterLogForwarderTemplate) {
-
-	// Build the pipelines from template
-	if len(logForwarderTemplate.Spec.Template.Outputs) < 1 {
-		r.log.V(3).Info("there is no output defined, skipping...")
-	} else {
-		for _, tOutput := range logForwarderTemplate.Spec.Template.Outputs {
-			if !strings.Contains(tOutput.Name, ManagedLogForwardTemplatePrefix) {
-				tOutput.Name = ManagedLogForwardTemplatePrefix + "-" + tOutput.Name
-			}
-			clusterLogForwarder.Spec.Outputs = append(clusterLogForwarder.Spec.Outputs, tOutput)
-		}
-	}
-}
-
-// BuildPipelines builds the pipeline array from the template and adds to the clean log forwarder
-func (r *ClusterLogForwarderTemplateReconciler) BuildPipelines(clusterLogForwarder *loggingv1.ClusterLogForwarder,
-	logForwarderTemplate *hlov1alpha1.ClusterLogForwarderTemplate) {
-
-	// Build the pipelines from template
-	if len(logForwarderTemplate.Spec.Template.Pipelines) < 1 {
-		r.log.V(3).Info("there is no pipeline defined, skipping...")
-	} else {
-		autoGenName := "auto-generated-name"
-		for x, tPpl := range logForwarderTemplate.Spec.Template.Pipelines {
-			if tPpl.Name == "" {
-				tPpl.Name = autoGenName + strconv.Itoa(x)
-			}
-			if !strings.Contains(tPpl.Name, ManagedLogForwardTemplatePrefix) {
-				tPpl.Name = ManagedLogForwardTemplatePrefix + "-" + tPpl.Name
-			}
-			clusterLogForwarder.Spec.Pipelines = append(clusterLogForwarder.Spec.Pipelines, tPpl)
-		}
-	}
-}
-
-// CleanUpClusterLogForwarder clear the related entries when the template deleted
-func (r *ClusterLogForwarderTemplateReconciler) CleanUpClusterLogForwarder(clf *loggingv1.ClusterLogForwarder) *loggingv1.ClusterLogForwarder {
-	newPipelines := clf.Spec.Pipelines[:0]
-	for _, ppl := range clf.Spec.Pipelines {
-		if !strings.Contains(ppl.Name, ManagedLogForwardTemplatePrefix) {
-			newPipelines = append(newPipelines, ppl)
-		}
-	}
-
-	newInputs := clf.Spec.Inputs[:0]
-	for _, input := range clf.Spec.Inputs {
-		if !strings.Contains(input.Name, ManagedLogForwardTemplatePrefix) {
-			newInputs = append(newInputs, input)
-		}
-	}
-	newOutputs := clf.Spec.Outputs[:0]
-	for _, output := range clf.Spec.Outputs {
-		if !strings.Contains(output.Name, ManagedLogForwardTemplatePrefix) {
-			newOutputs = append(newOutputs, output)
-		}
-	}
-	clf.Spec.Pipelines = newPipelines
-	clf.Spec.Inputs = newInputs
-	clf.Spec.Outputs = newOutputs
-	return clf
 }
 
 func eventPredicates() predicate.Predicate {
---
+++ pkg/clusterlogforwarder/clusterlogforwarder.go
@@ -0,0 +1,138 @@
+package clusterlogforwarder
+
+import (
+	"strconv"
+	"strings"
+
+	loggingv1 "github.com/openshift/cluster-logging-operator/apis/logging/v1"
+
+	"github.com/openshift/hypershift-logging-operator/api/v1alpha1"
+	"github.com/openshift/hypershift-logging-operator/pkg/consts"
+)
+
+// CleanUpClusterLogForwarder removes all the user managed forwarder rules from CLF
+func CleanUpClusterLogForwarder(clf *loggingv1.ClusterLogForwarder, keyword string) *loggingv1.ClusterLogForwarder {
+
+	newInputs := clf.Spec.Inputs[:0]
+	for _, input := range clf.Spec.Inputs {
+		if !strings.Contains(input.Name, keyword) {
+			newInputs = append(newInputs, input)
+		}
+	}
+
+	newOutputs := clf.Spec.Outputs[:0]
+	for _, output := range clf.Spec.Outputs {
+		if !strings.Contains(output.Name, keyword) {
+			newOutputs = append(newOutputs, output)
+		}
+	}
+
+	newPipelines := clf.Spec.Pipelines[:0]
+	for _, ppl := range clf.Spec.Pipelines {
+		if !strings.Contains(ppl.Name, keyword) {
+			newPipelines = append(newPipelines, ppl)
+		}
+	}
+	clf.Spec.Inputs = newInputs
+	clf.Spec.Outputs = newOutputs
+	clf.Spec.Pipelines = newPipelines
+
+	return clf
+}
+
+// BuildInputsFromTemplate builds the input array from the template
+func BuildInputsFromTemplate(template *v1alpha1.ClusterLogForwarderTemplate,
+	clf *loggingv1.ClusterLogForwarder) *loggingv1.ClusterLogForwarder {
+
+	if len(template.Spec.Template.Inputs) > 0 {
+		for _, input := range template.Spec.Template.Inputs {
+			if !strings.Contains(input.Name, consts.ProviderManagedRuleNamePrefix) {
+				input.Name = consts.ProviderManagedRuleNamePrefix + "-" + input.Name
+			}
+			clf.Spec.Inputs = append(clf.Spec.Inputs, input)
+		}
+	}
+
+	return clf
+}
+
+// BuildOutputsFromTemplate builds the output array from the template
+func BuildOutputsFromTemplate(template *v1alpha1.ClusterLogForwarderTemplate,
+	clf *loggingv1.ClusterLogForwarder) *loggingv1.ClusterLogForwarder {
+
+	if len(template.Spec.Template.Outputs) > 0 {
+		for _, output := range template.Spec.Template.Outputs {
+			if !strings.Contains(output.Name, consts.ProviderManagedRuleNamePrefix) {
+				output.Name = consts.ProviderManagedRuleNamePrefix + "-" + output.Name
+			}
+			clf.Spec.Outputs = append(clf.Spec.Outputs, output)
+		}
+	}
+
+	return clf
+}
+
+// BuildPipelinesFromTemplate builds the pipeline array from the template
+func BuildPipelinesFromTemplate(template *v1alpha1.ClusterLogForwarderTemplate,
+	clf *loggingv1.ClusterLogForwarder) *loggingv1.ClusterLogForwarder {
+
+	if len(template.Spec.Template.Pipelines) > 0 {
+		autoGenName := "auto-generated-name-"
+		for x, ppl := range template.Spec.Template.Pipelines {
+			if ppl.Name == "" {
+				ppl.Name = autoGenName + strconv.Itoa(x)
+			}
+			if !strings.Contains(ppl.Name, consts.ProviderManagedRuleNamePrefix) {
+				ppl.Name = consts.ProviderManagedRuleNamePrefix + "-" + ppl.Name
+			}
+			clf.Spec.Pipelines = append(clf.Spec.Pipelines, ppl)
+		}
+	}
+
+	return clf
+}
+
+// BuildInputsFromHLF builds the CLF inputs from the HLF
+func BuildInputsFromHLF(hlf *v1alpha1.HyperShiftLogForwarder, clf *loggingv1.ClusterLogForwarder) *loggingv1.ClusterLogForwarder {
+	if len(hlf.Spec.Inputs) > 0 {
+		for _, input := range hlf.Spec.Inputs {
+			if !strings.Contains(input.Name, consts.CustomerManagedRuleNamePrefix) {
+				input.Name = consts.CustomerManagedRuleNamePrefix + "-" + input.Name
+			}
+			clf.Spec.Inputs = append(clf.Spec.Inputs, input)
+		}
+	}
+	return clf
+}
+
+// BuildOutputsFromHLF builds the CLF outputs from the HLF
+func BuildOutputsFromHLF(hlf *v1alpha1.HyperShiftLogForwarder, clf *loggingv1.ClusterLogForwarder) *loggingv1.ClusterLogForwarder {
+	if len(hlf.Spec.Outputs) > 0 {
+		for _, output := range hlf.Spec.Outputs {
+			if !strings.Contains(output.Name, consts.CustomerManagedRuleNamePrefix) {
+				output.Name = consts.CustomerManagedRuleNamePrefix + "-" + output.Name
+			}
+			clf.Spec.Outputs = append(clf.Spec.Outputs, output)
+		}
+	}
+
+	return clf
+}
+
+// BuildPipelinesFromHLF builds the CLF pipelines from the HLF
+func BuildPipelinesFromHLF(hlf *v1alpha1.HyperShiftLogForwarder, clf *loggingv1.ClusterLogForwarder) *loggingv1.ClusterLogForwarder {
+	if len(hlf.Spec.Pipelines) > 0 {
+		autoGenName := "auto-generated-name-"
+		for i, ppl := range hlf.Spec.Pipelines {
+			if ppl.Name == "" {
+				ppl.Name = autoGenName + strconv.Itoa(i)
+			}
+			if !strings.Contains(ppl.Name, consts.CustomerManagedRuleNamePrefix) {
+				ppl.Name = consts.CustomerManagedRuleNamePrefix + "-" + ppl.Name
+			}
+			clf.Spec.Pipelines = append(clf.Spec.Pipelines, ppl)
+		}
+	}
+
+	return clf
+}
