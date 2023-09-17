b5da5ec085158c0ac8ed5fd124c230c5f1c43302
Samantha Jayasinghe <sjayasin@redhat.com>
2023-09-18 09:38:56 +1200

    add watch to sub cluster

--- main.go
+++ main.go
@@ -19,22 +19,29 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"os"
 
 	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
 	// to ensure that exec-entrypoint and run can make use of them.
+	corev1 "k8s.io/api/core/v1"
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
 
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client/config"
+	"sigs.k8s.io/controller-runtime/pkg/cluster"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
+	"github.com/go-logr/logr"
 	hlov1alpha1 "github.com/openshift/hypershift-logging-operator/api/v1alpha1"
 	"github.com/openshift/hypershift-logging-operator/controllers/clusterlogforwardertemplate"
 	"github.com/openshift/hypershift-logging-operator/controllers/hypershiftlogforwarder"
@@ -47,6 +54,7 @@ import (
 var (
 	scheme   = runtime.NewScheme()
 	setupLog = ctrl.Log.WithName("setup")
+	clusters = map[string]cluster.Cluster{}
 )
 
 func init() {
@@ -61,6 +69,28 @@ func init() {
 	utilruntime.Must(loggingv1.AddToScheme(scheme))
 
 	//+kubebuilder:scaffold:scheme
+
+	//temp setup sub-A cluster
+	restConfigSubA, err := createGuestKubeconfig(context.Background(), "ocm-stg-hs-two", setupLog)
+	if err != nil {
+		setupLog.Error(err, "getting guest cluster kubeconfig")
+	}
+	subAcluster, err := cluster.New(restConfigSubA)
+	if err != nil {
+		setupLog.Error(err, "creating guest cluster kubeconfig")
+	}
+	clusters["sub"] = subAcluster
+
+	// temp setup sub-b cluster
+	restConfigSubB, err := createGuestKubeconfig(context.Background(), "ocm-stg-hs-one", setupLog)
+	if err != nil {
+		setupLog.Error(err, "getting guest cluster kubeconfig")
+	}
+	subBcluster, err := cluster.New(restConfigSubB)
+	if err != nil {
+		setupLog.Error(err, "creating guest cluster kubeconfig")
+	}
+	clusters["sub-b"] = subBcluster
 }
 
 func main() {
@@ -101,13 +131,15 @@ func main() {
 		setupLog.Error(err, "unable to start manager")
 		os.Exit(1)
 	}
-	// list pods
-	setupLog.Info("Getting HCP")
-	hcpList, _ := GetHostedClusters()
-	for _, hcp := range hcpList {
-
-		setupLog.Info(hcp.Status.KubeConfig.Name)
+	// set up clusters
+	setupLog.Info("set up clusters ")
+	for name, cluster := range clusters {
+		clusterScheme := cluster.GetScheme()
+		utilruntime.Must(hlov1alpha1.AddToScheme(clusterScheme))
+		clusters[name] = cluster
+		mgr.Add(cluster)
 	}
+	_, err = hypershiftlogforwarder.NewHyperShiftLogForwarderReconciler(mgr, clusters)
 
 	setupLog.Info("Registering Components.")
 
@@ -119,13 +151,13 @@ func main() {
 		os.Exit(1)
 	}
 
-	if err = (&hypershiftlogforwarder.HyperShiftLogForwarderReconciler{
+	/*if err = (&hypershiftlogforwarder.HyperShiftLogForwarderReconciler{
 		Client: mgr.GetClient(),
 		Scheme: mgr.GetScheme(),
 	}).SetupWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create HyperShiftLogForwarderReconciler controller", "controller", "HyperShiftLogForwarderReconciler")
 		os.Exit(1)
-	}
+	}*/
 
 	//+kubebuilder:scaffold:builder
 
@@ -162,16 +194,49 @@ func GetHostedClusters() ([]hyperv1beta1.HostedControlPlane, error) {
 	return hcpList.Items, nil
 }
 
-/*func GetNamespaces(mgr ctrl.Manager) ([]corev1.Namespace, error) {
-	cl, err := client.New(config.GetConfigOrDie(), client.Options{})
-	if err != nil {
-		setupLog.Error(err, "failed to create client")
-		os.Exit(1)
+func createGuestKubeconfig(ctx context.Context, cpNamespace string, log logr.Logger) (*rest.Config, error) {
+
+	c, err := client.New(config.GetConfigOrDie(), client.Options{})
+
+	localhostKubeconfigSecret := &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "localhost-kubeconfig",
+			Namespace: cpNamespace,
+		},
 	}
-	namespaces := &corev1.NamespaceList{}
-	if err := cl.List(context.Background(), namespaces, &client.ListOptions{Namespace: ""}); err != nil {
-		return nil, err
+	if err := c.Get(ctx, client.ObjectKeyFromObject(localhostKubeconfigSecret), localhostKubeconfigSecret); err != nil {
+		return nil, fmt.Errorf("failed to get hostedcluster localhost kubeconfig: %w", err)
+	}
+	kubeconfigFile, err := os.CreateTemp(os.TempDir(), "kubeconfig-")
+	if err != nil {
+		return nil, fmt.Errorf("failed to create tempfile for kubeconfig: %w", err)
+	}
+	defer func() {
+		if err := kubeconfigFile.Sync(); err != nil {
+			log.Error(err, "Failed to sync temporary kubeconfig file")
+		}
+		if err := kubeconfigFile.Close(); err != nil {
+			log.Error(err, "Failed to close temporary kubeconfig file")
+		}
+	}()
+	localhostKubeconfig, err := clientcmd.Load(localhostKubeconfigSecret.Data["kubeconfig"])
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse localhost kubeconfig: %w", err)
+	}
+	if len(localhostKubeconfig.Clusters) == 0 {
+		return nil, fmt.Errorf("no clusters found in localhost kubeconfig")
 	}
 
-	return namespaces.Items, nil
-}*/
+	//for k := range localhostKubeconfig.Clusters {
+	//	localhostKubeconfig.Clusters[k].Server = fmt.Sprintf("https://localhost:%d", localPort)
+	//}
+	localhostKubeconfigYaml, err := clientcmd.Write(*localhostKubeconfig)
+	restConfig, err := clientcmd.RESTConfigFromKubeConfig(localhostKubeconfigYaml)
+	if err != nil {
+		return nil, fmt.Errorf("failed to serialize localhost kubeconfig: %w", err)
+	}
+
+	return restConfig, nil
+}
+
+// oc get secret 267hpof1a3bpgjildo6locrmqhmd3ml3-kubeconfig -n ocm-staging-267hpof1a3bpgjildo6locrmqhmd3ml3-hcpeast2latest --as backplane-cluster-admin | grep kubeconfig
